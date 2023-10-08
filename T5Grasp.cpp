672fb6b2f3b84d3b35da12b76ce4d2ed35fd8e2d
esuazo2601 <esteban.suazo.m2601@gmail.com>
2023-10-08 15:47:15 -0300

    esta casi listooo

--- T5Grasp.cpp
+++ T5Grasp.cpp
@@ -56,38 +56,76 @@ string genera_random_sol(int largo){
     return sol;
 }
 
-/* pair<string,int> busqueda_local(string sol_in){
+pair<string, int> greedy(int largo, vector<string> entrada, int num_intentos) {
+    string mejor_solucion = genera_random_sol(largo);
+    int mejor_costo = hamming_cuadrado_a(mejor_solucion, entrada);
 
-}
- */
-pair<string, int> greedy(int largo, vector<string> entrada, long tiempo_max_ms) {
-    string sol_ac = genera_random_sol(largo);
-    int costo_ac = hamming_cuadrado_a(sol_ac, entrada);
-    int mejor_costo = costo_ac;
-    string sol_final = sol_ac;
-    long long start_time = getCurrentTimeMillis();
+    for (int intento = 0; intento < num_intentos; intento++) {
+        string sol_actual = genera_random_sol(largo);
+        int costo_actual = hamming_cuadrado_a(sol_actual, entrada);
 
-    while (getCurrentTimeMillis() - start_time < tiempo_max_ms) {
-        int index = rand() % largo;
-        string nueva_sol = sol_final;
-        nueva_sol[index] = "AGTC"[rand() % 4];
-        int nuevo_costo = hamming_cuadrado_a(nueva_sol, entrada);
+        while (true) {
+            int index = rand() % largo;
+            string nueva_sol = sol_actual;
+            nueva_sol[index] = "AGTC"[rand() % 4];
+            int nuevo_costo = hamming_cuadrado_a(nueva_sol, entrada);
 
-        if (nuevo_costo < mejor_costo) {
-            mejor_costo = nuevo_costo;
-            sol_final = nueva_sol;
-            cout << "Nueva mejor solución encontrada -> " << " Costo: " << mejor_costo << endl;
+            if (nuevo_costo < costo_actual) {
+                costo_actual = nuevo_costo;
+                sol_actual = nueva_sol;
+            } else {
+                break;  // Salir si no hay mejora
+            }
+        }
+
+        if (costo_actual < mejor_costo) {
+            mejor_costo = costo_actual;
+            mejor_solucion = sol_actual;
         }
     }
 
-    return make_pair(sol_final, mejor_costo);
+    return make_pair(mejor_solucion, mejor_costo);
 }
 
 
-/* string GRASP(int tiempo_max){
+pair<string, int> busqueda_local(const string& solucion_actual, const vector<string>& entrada, int num_iteraciones) {
+    string mejor_solucion = solucion_actual;
+    int mejor_costo = hamming_cuadrado_a(solucion_actual, entrada);
 
-}  */
+    for (int iteracion = 0; iteracion < num_iteraciones; iteracion++) {
+        string sol_actual = mejor_solucion;
+        int costo_actual = mejor_costo;
 
+        for (int i = 0; i < solucion_actual.length(); i++) {
+            string nueva_solucion = sol_actual;
+            nueva_solucion[i] = "AGTC"[rand() % 4];
+            int nuevo_costo = hamming_cuadrado_a(nueva_solucion, entrada);
+
+            if (nuevo_costo < costo_actual) {
+                costo_actual = nuevo_costo;
+                sol_actual = nueva_solucion;
+            }
+        }
+
+        if (costo_actual < mejor_costo) {
+            mejor_costo = costo_actual;
+            mejor_solucion = sol_actual;
+        }
+    }
+
+    return make_pair(mejor_solucion, mejor_costo);
+}
+
+
+pair<string, int> grasp(int largo, vector<string> entrada, long tiempo_max_ms) {
+    pair<string, int> solucion_greedy = greedy(largo, entrada, 10);
+    string sol_greedy = solucion_greedy.first;
+
+    // Aplicar búsqueda local a la solución Greedy
+    pair<string, int> solucion_final = busqueda_local(sol_greedy, entrada,10);
+
+    return solucion_final;
+}
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
@@ -106,13 +144,26 @@ int main(int argc, char* argv[]) {
     long long start_time = getCurrentTimeMillis();
     long long end_time = start_time + tiempo_max_ms; // Calcula el tiempo de finalización
 
+    pair<string, int> mejor_solucion;
+    bool primera_solucion = true;
+
     while (getCurrentTimeMillis() < end_time) {
-        pair<string, int> res = greedy(len, entrada, tiempo_max_ms);
-        cout << "Solución encontrada es: " << res.first << " Costo: " << res.second << endl;
+        pair<string, int> solucion_actual = grasp(len, entrada, tiempo_max_ms);
+
+        if (primera_solucion || solucion_actual.second < mejor_solucion.second) {
+            mejor_solucion = solucion_actual;
+            primera_solucion = false;
+
+            long long tiempo_actual = getCurrentTimeMillis();
+            cout << "Tiempo: " << (tiempo_actual - start_time) / 1000.0 << " segundos, Costo: " << mejor_solucion.second << endl;
+        }
     }
-    
+
+    cout << "Mejor solución encontrada es: " << mejor_solucion.first << " Costo: " << mejor_solucion.second << endl;
+
     return 0;
 }
+
 
 
 
